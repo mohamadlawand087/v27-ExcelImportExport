dd6503123cb02f4445a54aa499e4a9fa43ed9f98
mohamad <mohamad.lawand@outlook.com>
2021-04-26 11:20:14 +0100

    Initial commit

---
+++ SampleExcel/Models/User.cs
@@ -0,0 +1,9 @@
+namespace SampleExcel.Models
+{
+    public class User
+    {
+        public string Name {get;set;}
+        public string Email {get;set;}
+        public string Phone {get;set;}
+    }
+}
